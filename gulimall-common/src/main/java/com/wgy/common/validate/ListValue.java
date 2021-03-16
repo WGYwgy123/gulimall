8b84b1e2db93b3cca7150a0a303fe58ebf587935
wgy <709581924@qq.com>
2021-03-16 19:55:15 +0800

    删除oss

--- gulimall-common/src/main/java/com/wgy/common/validate/ListValue.java
+++ gulimall-common/src/main/java/com/wgy/common/validate/ListValue.java
@@ -16,11 +16,14 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * To change this template use File | Settings | File Templates.
  **/
 @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
-@Retention(RUNTIME)
-@Documented
+// 说明了Annotation所修饰的对象范围
+@Retention(RUNTIME) // 注解按生命周期来划分可分为3类：class（class文件） source（源文件） runtime（jvm加载完仍然存在）
+@Documented // 做标识
 @Constraint(validatedBy = {ListValueConstrainValidator.class})
 public @interface ListValue {
 
+    // @Constraint(validatedBy = {ListValueConstrainValidator.class})
+    // 代表注解的处理逻辑是 使用后面这个class来进行校验
     String message() default "{com.wgy.common.validate.ListValue.message}";
 
     Class<?>[] groups() default {};
